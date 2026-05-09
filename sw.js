64020c75747cb879effb5db07afac357588061f2
rodrigorodrigues0311-rgb <rodrigo.rodrigues0311@gmail.com>
2026-05-08 22:53:09 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,30 +1,28 @@
-const CACHE_NAME = 'csn-automacao-v1';
-const ASSETS = [
+const CACHE = 'csn-rel-v3';
+const FILES = [
   './',
   './index.html',
   './manifest.json',
+  './icons/icon-72x72.png',
   './icons/icon-192x192.png',
   './icons/icon-512x512.png'
 ];
 
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(c => c.addAll(ASSETS))
-  );
   self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES).catch(()=>{})));
 });
 
 self.addEventListener('activate', e => {
   e.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    )
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
   );
-  self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
+    caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match('./index.html')))
   );
 });
