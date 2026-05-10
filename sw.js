b7c44d5ca8e8d762643f02f4fa42a7ef7e73419b
rodrigorodrigues0311-rgb <rodrigo.rodrigues0311@gmail.com>
2026-05-09 22:14:32 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-// v1778373916
-const CACHE='csn-1778373916';
-const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
+// v1778375211
+const CACHE='csn-1778375211';
+const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png','./lima-logo.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS).catch(()=>{})));});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
 self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));});
