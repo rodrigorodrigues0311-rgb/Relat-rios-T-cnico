793a77f1c66ba2f9afc206aff372ab4c650aa732
rodrigorodrigues0311-rgb <rodrigo.rodrigues0311@gmail.com>
2026-05-11 19:36:22 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,8 +1,7 @@
-// v1778534152 - CSN Automacao Colaborativo
-const CACHE='csn-1778534152';
+// v1778538772
+const CACHE='csn-1778538772';
 const BASE='/Relat-rios-T-cnico/';
-const ASSETS=[BASE, BASE+'index.html', BASE+'manifest.json',
-  BASE+'icon-192.png', BASE+'icon-512.png', BASE+'lima-logo.png'];
+const ASSETS=[BASE,BASE+'index.html',BASE+'manifest.json',BASE+'icon-192.png',BASE+'icon-512.png',BASE+'lima-logo.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS).catch(()=>{})));});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
 self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));});
