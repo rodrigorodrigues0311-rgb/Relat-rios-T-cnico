c069a62b96ef85335597a5129dbb9eabdaa9d838
rodrigorodrigues0311-rgb <rodrigo.rodrigues0311@gmail.com>
2026-05-11 19:51:36 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// v1778538772
-const CACHE='csn-1778538772';
+// v1778539589
+const CACHE='csn-1778539589';
 const BASE='/Relat-rios-T-cnico/';
 const ASSETS=[BASE,BASE+'index.html',BASE+'manifest.json',BASE+'icon-192.png',BASE+'icon-512.png',BASE+'lima-logo.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS).catch(()=>{})));});
