03b5825062d162cb588f39bf55b871eb7a115e46
rodrigorodrigues0311-rgb <rodrigo.rodrigues0311@gmail.com>
2026-05-11 22:37:18 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// v1778548892
-const CACHE='csn-1778548892';
+// v1778549694
+const CACHE='csn-1778549694';
 self.addEventListener('install',function(e){self.skipWaiting();});
 self.addEventListener('activate',function(e){e.waitUntil(caches.keys().then(function(ks){return Promise.all(ks.map(function(k){return caches.delete(k);}));}).then(function(){return self.clients.claim();}));});
 self.addEventListener('fetch',function(e){e.respondWith(fetch(e.request).catch(function(){return new Response('',{status:408})}));});
